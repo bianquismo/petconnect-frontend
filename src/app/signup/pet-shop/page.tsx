3fcd02a627d1cbf21c72e5bf3e688e914bc7a5f7
bianquismo <b.santosdias@outlook.com>
2024-05-22 22:22:40 -0300

    feat(react-hook-form): :sparkles: Add simple react-hook-form

--- src/app/signup/pet-shop/page.tsx
+++ src/app/signup/pet-shop/page.tsx
@@ -1,39 +1,50 @@
-import Image from 'next/image';
+"use client";
+
 import { Button } from '@/components/Button';
 import { Input } from '@/components/Input';
 import Link from 'next/link';
 
+import { useForm, SubmitHandler } from "react-hook-form";
+
+export type Inputs = {
+    name: string,
+    address: string,
+    phoneNumber: string,
+    openHours: string,
+    petshopLogo: FileList,
+};
+
 export default function SignupPetShopPage() {
+    const { register, handleSubmit, watch, formState: { errors } } = useForm<Inputs>();
+    const onSubmit: SubmitHandler<Inputs> = data => console.log(data);
+
     return (
         <div className="flex flex-col items-center justify-center min-h-screen p-4 bg-gray-100">
             <div className="w-full max-w-md">
                 <h1 className={`text-center text-[#BAD36D] font-light text-2xl mb-8`}>
                     CADASTRO DE PET SHOP
                 </h1>
-                <form className="space-y-6">
-                    <Input label='Nome' htmlFor="name" type="text"
-                        id="name"
-                        name="name" placeholder="Petz - Seu Campeão"
+                <form onSubmit={handleSubmit(onSubmit)} className="space-y-2">
+                    <Input label='Nome' htmlFor="name" type="text" name="name"
+                        id="name" placeholder="Petz - Seu Campeão" register={register}
                         required />
-                    <Input label='Endereço' htmlFor="address" type="text"
-                        id="address"
-                        name="address" placeholder="Av Vicente Rao, 1700, CEP.: 04662-003"
+                    <Input label='Endereço' htmlFor="address" type="text" name="address"
+                        id="address" placeholder="Av Vicente Rao, 1700, CEP.: 04662-003" register={register}
                         required />
-                    <Input label='Telefone' htmlFor="phoneNumber" type="text"
+                    <Input label='Telefone' htmlFor="phoneNumber" type="text" name="phoneNumber"
                         id="phoneNumber"
-                        name="phoneNumber" placeholder="(11) 91234-5678"
+                        register={register} placeholder="(11) 91234-5678"
                         required />
-                    <Input label='Horário de Funcionamento' htmlFor="openHours" type="text"
+                    <Input label='Horário de Funcionamento' htmlFor="openHours" type="text" name="openHours"
                         id="openHours"
-                        name="openHours" placeholder="08:00 às 19:00"
+                        register={register} placeholder="08:00 às 19:00"
                         required />
-                    <Input label='Logotipo da Empresa' htmlFor="petshopLogo" type="file"
+                    <Input label='Logotipo da Empresa' htmlFor="petshopLogo" type="file" name="petshopLogo"
                         id="petshopLogo"
-                        name="petshopLogo" placeholder="nenhum arquivo selecionado"
+                        register={register} placeholder="nenhum arquivo selecionado"
                         required />
                     <Button
-                        type="submit"
-                    >
+                        type="submit">
                         cadastrar
                     </Button>
                 </form>
--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,12 +1,16 @@
+import { Inputs } from '@/app/signup/pet-shop/page';
 import React, { FC, InputHTMLAttributes } from 'react'
+import { FieldValues, UseFormRegister } from 'react-hook-form'
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
-    label?: string,
-    htmlFor?: string
+    label?: string;
+    htmlFor?: string;
+    register: UseFormRegister<Inputs>;
+    name: keyof Inputs;
 }
 
 export const Input: FC<InputProps> =
-    ({ label, htmlFor, ...props }) => {
+    ({ label, htmlFor, register, name, ...props }) => {
         const primaryGreen = "#BAD36D"
 
         return (
@@ -14,7 +18,9 @@ export const Input: FC<InputProps> =
                 <label htmlFor={htmlFor} className="block text-sm font-semibold text-[#353535] py-3">
                     {label}
                 </label>
-                <input {...props}
+                <input
+                    {...register(name)}
+                    {...props}
                     className={`mt-1 block w-full px-4 py-2 border rounded-md shadow-sm focus:outline-none focus:${primaryGreen} focus:${primaryGreen} text-lg placeholder-[#BFBFBF] border-[${primaryGreen}] text-[#949494]`}
                     placeholder="Enter your email"
                     required
