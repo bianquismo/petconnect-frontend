f7603f50e356119e8ac02deda655d45fab435302
bianquismo <b.santosdias@outlook.com>
2024-05-26 15:59:01 -0300

    feat(PetShopList): :sparkles: Fix styles for the list

--- src/app/petshops/page.tsx
+++ src/app/petshops/page.tsx
@@ -2,12 +2,37 @@ import { Footer } from "@/components/Footer";
 import { Navbar } from "@/components/Navbar";
 import { PetShopList } from "@/components/Petshop";
 
-export default function PetShopsPage() {
+interface PetShopPageProps {
+    petShops: []
+}
+
+async function getData() {
+    const res = await fetch('http://localhost:8080/pet_shops', {
+        cache: 'no-store'
+    })
+    // The return value is *not* serialized
+    // You can return Date, Map, Set, etc.
+
+    const data = await res.json();
+
+    console.log(data.data);
+
+    if (!res.ok) {
+        // This will activate the closest `error.js` Error Boundary
+        throw new Error('Failed to fetch data')
+    }
+
+    return data;
+}
+
+export default async function PetShopsPage() {
+    const data = await getData();
+
     return (
-        <div className="flex flex-col max-h-screen bg-gray-100">
+        <div className="flex flex-col min-h-screen h-screen bg-gray-100">
             <Navbar />
 
-            <PetShopList />
+            <PetShopList petShops={data} />
 
             <Footer />
         </div>
--- src/components/Petshop/index.tsx
+++ src/components/Petshop/index.tsx
@@ -3,58 +3,18 @@ import Link from "next/link";
 import { FC } from "react"
 import { FaStar } from "react-icons/fa";
 
-const petShops = [
-    {
-        imgSrc: '/assets/petshop_listitem_1.png',
-        name: 'Pet Shop 1',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-        id: 1,
-        rating: 4
-    },
-    {
-        imgSrc: '/assets/petshop_listitem_2.png',
-        name: 'Pet Shop 2',
-        description: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-        id: 2,
-        rating: 5
-    },
-    {
-        imgSrc: '/assets/petshop_listitem_3.png',
-        name: 'Pet Shop 3',
-        description: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.',
-        id: 3,
-        rating: 3
-    },
-    {
-        imgSrc: '/assets/petshop_listitem_4.png',
-        name: 'Pet Shop 4',
-        description: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.',
-        id: 4,
-        rating: 3
-    },
-    {
-        imgSrc: '/assets/petshop_listitem_5.png',
-        name: 'Pet Shop 5',
-        description: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.',
-        id: 5,
-        rating: 3
-    },
-    {
-        imgSrc: '/assets/petshop_listitem_6.png',
-        name: 'Pet Shop 6',
-        description: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.',
-        id: 6,
-        rating: 3
-    },
-];
+interface PetShopListProps {
+    petShops: []
+}
 
+export const PetShopList: FC<PetShopListProps> = ({ petShops }) => {
+    console.log(petShops);
 
-export const PetShopList: FC = () => {
     return (
         <div className="flex-1 overflow-y-auto p-2">
-            {petShops.map((item, index) => (
+            {petShops ? petShops.map((item, index) => (
                 <PetShopItem item={item} key={index} />
-            ))}
+            )) : <>no results</>}
         </div>
     )
 }
@@ -63,20 +23,22 @@ interface PetShopItemProps {
     item: {
         imgSrc: string;
         name: string;
-        description: string;
+        address: string;
         rating: number;
         id: number;
     }
 }
 
 const PetShopItem: FC<PetShopItemProps> = ({ item }) => {
-    const { imgSrc, name, description, rating, id } = item;
+    const { imgSrc, name, address, rating, id } = item;
+    console.log(item);
+    
     return (
         <>
             <Link className="flex items-center p-2 bg-zinc-100 shadow-lg rounded-md border-slate-900 mb-4" href={`/petshops/${id}`}>
                 <div className="relative w-20 h-20 flex-shrink-0">
                     <Image
-                        src={imgSrc}
+                        src={"/assets/petshop_listitem_1.png"}
                         alt={name}
                         layout="fill"
                         objectFit="cover"
@@ -85,7 +47,7 @@ const PetShopItem: FC<PetShopItemProps> = ({ item }) => {
                 </div>
                 <div className="flex-1 ml-4 mr-4">
                     <h3 className="text-md font-bold text-black">{name}</h3>
-                    <p className="text-xs text-gray-600">{description}</p>
+                    <p className="text-xs text-gray-600">{address}</p>
                 </div>
                 <div className=" relative top-0 right-0 ml-auto">
                     {Array.from({ length: 5 }).map((_, index) => (
